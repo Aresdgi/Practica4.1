0c08400d8da39c82c5f062982a42c42ce4e5bf3b
aresdgi <aresdominguezgil@gmail.com>
2024-12-13 10:12:40 +0100

    añadido configuraciones a package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,16 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs", // Indica que estás usando CommonJS
+      globals: {
+        ...globals.node, // Habilita las variables globales de Node.js
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
